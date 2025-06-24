dad40fd15a159b2e177ff9077abbe7b2dd7832d6
yael-shalom <yael7147757@gmail.com>
2025-06-24 18:33:55 +0300

    remove console log from ApartmentsService constructor

--- src/app/shared/services/apartments.service.ts
+++ src/app/shared/services/apartments.service.ts
@@ -9,8 +9,8 @@ import { Apartment } from '../models/apartment';
 export class ApartmentsService {
   private _url: string = 'https://data.gov.il/api/3/action/datastore_search?resource_id=d6d2046b-ccba-4d09-8778-ee9aa57cdf0c';
   private _http = inject(HttpClient);
-  private _list$: BehaviorSubject<Apartment[]> = new BehaviorSubject<Apartment[]>([]);
 
+  private _list$: BehaviorSubject<Apartment[]> = new BehaviorSubject<Apartment[]>([]);
   public get list$(): Observable<Apartment[]> {
     return this._list$ as Observable<Apartment[]>;
   }
@@ -18,7 +18,6 @@ export class ApartmentsService {
   constructor() {
     this.init().subscribe(apartments => {
       this._list$.next(apartments);
-      console.log('Apartments fetched:', apartments);
     });
   }
 
