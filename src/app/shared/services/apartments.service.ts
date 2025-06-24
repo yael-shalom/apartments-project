88e91c8487a2c9c3ba62172ea514100ecfa70973
yael-shalom <yael7147757@gmail.com>
2025-06-24 17:38:33 +0300

    refactor ApartmentsService to use BehaviorSubject for apartment list management

--- src/app/shared/services/apartments.service.ts
+++ src/app/shared/services/apartments.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Apartment } from '../models/apartment';
 
 @Injectable({
@@ -9,8 +9,20 @@ import { Apartment } from '../models/apartment';
 export class ApartmentsService {
   private _url: string = 'https://data.gov.il/api/3/action/datastore_search?resource_id=d6d2046b-ccba-4d09-8778-ee9aa57cdf0c';
   private _http = inject(HttpClient);
+  private _list$: BehaviorSubject<Apartment[]> = new BehaviorSubject<Apartment[]>([]);
 
-  public getApartments(): Observable<Apartment[]> {
+  public get list$(): Observable<Apartment[]> {
+    return this._list$ as Observable<Apartment[]>;
+  }
+
+  constructor() {
+    this.init().subscribe(apartments => {
+      this._list$.next(apartments);
+      console.log('Apartments fetched:', apartments);
+    });
+  }
+
+  private init(): Observable<Apartment[]> {
     return this._http.get<{ result: { records: Apartment[] } }>(this._url).pipe(map(x => x.result.records));
   }
 }
