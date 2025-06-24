f38cc4eb812db5da7ad7c055fc228a3f213533a1
yael-shalom <yael7147757@gmail.com>
2025-06-24 16:00:04 +0300

    add PrimeNG dependencies and configure animations in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideAnimationsAsync(),
+  providePrimeNG({
+    theme: {
+      preset: Aura
+    }
+  }),]
 };
